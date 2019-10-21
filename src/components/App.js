f99713ba564f062151216d6307139051ca8d3595
rendrap <rendrapradana@gmail.com>
2019-10-21 21:47:13 +0700

    adding data.json

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/App.css';
 
 import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments />
-              <SearchAppointments />
-              <ListAppointments />
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      myAppointments: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          return item;
+        })
+
+        this.setState({
+          myAppointments: apts
+        });
+
+      });
+  }
+
+  render () {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
+
 }
 
 export default App;
