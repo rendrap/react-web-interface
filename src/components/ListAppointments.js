ba93e0a334333cc88c30660be97ebb45697f9066
rendrap <rendrapradana@gmail.com>
2019-10-22 05:24:26 +0700

    pass props to ListAppointmens components

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 
 class ListAppointments extends Component {
-  render () {
+    render() {
+        return (
+            <div className="appointment-list item-list mb-3">
+        { this.props.appointments.map( (item, index) => (
+            <div key= {index} className="pet-item col media py-3">
+              <div className="mr-3">
+                <button className="pet-delete btn btn-sm btn-danger">X</button>
+              </div>
 
-    const listItems = this.props.appointments.map((item, index) => (
-      <div>
-        <div key={index}>{ item.petName }</div>
-        <div key={index}>{ item.ownerName }</div>
+              <div className="pet-info media-body">
+                <div className="pet-head d-flex">
+                  <span className="pet-name">{item.petName}</span>
+                  <span className="apt-date ml-auto">{item.aptDate}</span>
+                </div>
+
+                <div className="owner-name">
+                  <span className="label-item">Owner: </span>
+                  <span>{item.ownerName}</span>
+                </div>
+                <div className="apt-notes">{item.aptNotes}</div>
+              </div>
+            </div>
+        ))}
       </div>
-      ));
-
-    return <div>{listItems}</div>;
-  }
+        );
+    }
 }
 
 export default ListAppointments;
