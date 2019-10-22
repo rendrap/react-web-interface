016b9266e98f7e9901a2311dcdbc3d56c6b29623
rendrap <rendrapradana@gmail.com>
2019-10-22 07:51:54 +0700

    Updated on: 2019-10-22 - 07:51:52

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -4,26 +4,26 @@ class ListAppointments extends Component {
     render() {
         return (
             <div className="appointment-list item-list mb-3">
-        { this.props.appointments.map( (item, index) => (
-            <div key= {index} className="pet-item col media py-3">
-              <div className="mr-3">
-                <button className="pet-delete btn btn-sm btn-danger">X</button>
-              </div>
+                { this.props.appointments.map( (item, index) => (
+                    <div key= {index} className="pet-item col media py-3">
+                      <div className="mr-3">
+                        <button className="pet-delete btn btn-sm btn-danger">X</button>
+                      </div>
 
-              <div className="pet-info media-body">
-                <div className="pet-head d-flex">
-                  <span className="pet-name">{item.petName}</span>
-                  <span className="apt-date ml-auto">{item.aptDate}</span>
-                </div>
+                      <div className="pet-info media-body">
+                        <div className="pet-head d-flex">
+                          <span className="pet-name">{item.petName}</span>
+                          <span className="apt-date ml-auto">{item.aptDate}</span>
+                        </div>
 
-                <div className="owner-name">
-                  <span className="label-item">Owner: </span>
-                  <span>{item.ownerName}</span>
-                </div>
-                <div className="apt-notes">{item.aptNotes}</div>
-              </div>
-            </div>
-        ))}
+                        <div className="owner-name">
+                          <span className="label-item">Owner: </span>
+                          <span>{item.ownerName}</span>
+                        </div>
+                        <div className="apt-notes">{item.aptNotes}</div>
+                      </div>
+                    </div>
+                ))}
       </div>
         );
     }
